809a464ff9bc71c7f64d18a074b139bdcb217850
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-02-26 21:24:44 +0300

    editNoteView ui done

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app_with_local_storge/const.dart';
 import 'package:notes_app_with_local_storge/views/widgets/custom_app_bar.dart';
+import 'package:notes_app_with_local_storge/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -22,8 +23,16 @@ class EditNoteViewBody extends StatelessWidget {
             icon: Icons.check,
           ),
           SizedBox(
-            height: heightScreen * .01,
+            height: heightScreen * .04,
           ),
+          const CustomTextField(hintText: "Title"),
+          SizedBox(
+            height: heightScreen * 0.03,
+          ),
+          const CustomTextField(
+            hintText: "Content",
+            maxLines: 5,
+          )
         ],
       ),
     );
