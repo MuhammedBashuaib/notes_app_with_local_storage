d861595f4cd804483697be2315cb15e1a8c4a6a9
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-02-28 19:08:44 +0300

    ADD add note cubit to add note bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:notes_app_with_local_storge/const.dart';
+import 'package:notes_app_with_local_storge/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app_with_local_storge/models/note_model.dart';
 import 'package:notes_app_with_local_storge/views/edit_note_view.dart';
 import 'package:notes_app_with_local_storge/views/notes_view.dart';
@@ -20,17 +22,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     initializeHWFSize(context);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: "Poppins",
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: ((context) => AddNoteCubit())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: "Poppins",
+        ),
+        routes: {
+          MyRoutes.notesView: (context) => const NotesView(),
+          MyRoutes.editNoteView: (context) => const EditNoteView(),
+        },
+        initialRoute: MyRoutes.notesView,
       ),
-      routes: {
-        MyRoutes.notesView: (context) => const NotesView(),
-        MyRoutes.editNoteView: (context) => const EditNoteView(),
-      },
-      initialRoute: MyRoutes.notesView,
     );
   }
 }
--- lib/views/widgets/add_note_buttom_sheet.dart
+++ lib/views/widgets/add_note_buttom_sheet.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app_with_local_storge/const.dart';
+import 'package:notes_app_with_local_storge/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app_with_local_storge/views/widgets/add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -11,8 +14,23 @@ class AddNoteBottomSheet extends StatelessWidget {
       padding: EdgeInsets.symmetric(
         horizontal: widthScreen * .02,
       ),
-      child: const SingleChildScrollView(
-        child: AddNoteForm(),
+      child: SingleChildScrollView(
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFailure) {
+              print("failure is ${state.erorrMessage}");
+            }
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoding ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+        ),
       ),
     );
   }
