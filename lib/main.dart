4655c8f21e2972bf6a39780b602749709f29ee61
Muhammed Bashuaib <muhammedbashaib470@gmail.com>
2024-02-29 08:37:02 +0300

    CREATE notes cubit and Create state

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:notes_app_with_local_storge/const.dart';
-import 'package:notes_app_with_local_storge/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app_with_local_storge/models/note_model.dart';
 import 'package:notes_app_with_local_storge/simple_bloc_observer.dart';
 import 'package:notes_app_with_local_storge/views/edit_note_view.dart';
